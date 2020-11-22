b1efbbbc6c57f2c681bafcdc1b098b9b857566c1
miizzo <miizzo.js@mail.ru>
2020-11-22 19:48:34 +0300

    chore(backend): updated app module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getMetadataArgsStorage } from 'typeorm';
-import { join } from 'path'
 
-import { AppController } from './app.controller'
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 
@@ -16,8 +14,8 @@ import { PostsModule } from './posts/posts.module';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: '123456',
-      database: 'test',
+      password: '11197811aS@',
+      database: 'nxngnestblog',
       entities: getMetadataArgsStorage().tables.map(tbl => tbl.target),
       synchronize: true,
       logging: 'all',
